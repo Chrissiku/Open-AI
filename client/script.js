b4c74f3baa792163e8fb3c4dff1c170b3c01c246
Chris Siku <chrissiku5@gmail.com>
2022-12-28 14:24:42 +0200

    Deploy the server and and server link

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://cicada.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
